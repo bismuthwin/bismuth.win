740d6c3443c0942349123db2f3f2333db7bbbfa5
Antonin <flowythe@gmail.com>
2025-08-17 21:07:24 +0200

    fixed temp team page

--- src/app/thecrewmates/page.tsx
+++ src/app/thecrewmates/page.tsx
@@ -1,22 +1,34 @@
 "use client";
 
 import Link from "next/link";
-import { Button, Container, Flex, Image, rem, Title } from "@mantine/core";
+import { Button, Container, Flex, rem, Text, Title } from "@mantine/core";
 
 import { Routes } from "@/constants";
+import { LogoInBackground } from "@/modules/layout";
 
-export default function Home() {
+export default function TheTeamPage() {
     return (
         <>
-            <Flex pos={"absolute"} w={"100%"} h={"100%"} style={{ flexGrow: 1 }} align={"center"} justify={"center"}>
-                <Image src={"/img/bismuth-gray.svg"} alt={"bismuth logo"} w={"95vh"} opacity={0.025} />
-            </Flex>
-            <Container fluid h={"20dvh"} w={"100%"}>
-                <Flex w={"100%"} h={"100%"} justify={"center"} align={"left"} direction={"column"} gap={"x2"}>
+            <LogoInBackground />
+            <Container fluid h={"100dvh"} w={"100%"}>
+                <Flex direction={"column"} gap={"sm"} mb={"xl"}>
                     <Title size={rem(45)}>Really basic contact page because we need one quickly:</Title>
-                    <Title size={rem(25)}>Flowyan: <a href="https://steamcommunity.com/id/flowyan">Steam</a></Title>
-                    <Title size={rem(25)}>Chutoy: <a href="https://steamcommunity.com/id/Chutoy22">Steam</a></Title>
+                    <Text size={rem(25)}>
+                        Flowyan:{" "}
+                        <Link href="https://steamcommunity.com/id/flowyan" target="_blank">
+                            Steam
+                        </Link>
+                    </Text>
+                    <Text size={rem(25)}>
+                        Chutoy:{" "}
+                        <Link href="https://steamcommunity.com/id/Chutoy22" target="_blank">
+                            Steam
+                        </Link>
+                    </Text>
                 </Flex>
+                <Button size="xl" href={Routes.HOME_PAGE} component={Link}>
+                    go BACK
+                </Button>
             </Container>
         </>
     );
